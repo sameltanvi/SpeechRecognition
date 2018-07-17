c07f62cec39cc4d6823a5194c1ddb1fbeca7b722
sameltanvi <sameltanvi@gmail.com>
2018-07-17 11:58:47 +0100

    Invoking Camera on recognizing STOP

--- app/src/main/java/com/androidkt/tfaudio/MainActivity.java
+++ app/src/main/java/com/androidkt/tfaudio/MainActivity.java
@@ -1,7 +1,6 @@
 package com.androidkt.tfaudio;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.AudioFormat;
@@ -13,11 +12,8 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -90,6 +86,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         label = findViewById(R.id.label);
         label2 = findViewById(R.id.label2);
+        takePictureButton = (Button) findViewById(R.id.button_image);
+        imageView = (ImageView) findViewById(R.id.imageview);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            takePictureButton.setEnabled(false);
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+        }
+
 
         // Load the labels for the model, but only display those that don't start
         // with an underscore.
@@ -147,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
             startRecording();
             startRecognition();
             //Toast.makeText(getApplicationContext(), "HELLO", Toast.LENGTH_SHORT).show();
-            //takePictureButton.setEnabled(true);
+            takePictureButton.setEnabled(true);
         }
     }
 
@@ -312,7 +316,9 @@ public class MainActivity extends AppCompatActivity {
                                 switch (result.foundCommand) {
 
                                     case "stop":
-                                        Toast.makeText(getApplicationContext(), "Word is stop, button will be clicked", Toast.LENGTH_LONG).show();
+                                        Toast.makeText(getApplicationContext(), "Word is stop, button will be clicked", Toast.LENGTH_SHORT).show();
+                                    takePicture();
+                                        break;
 
                                     case "go":
                                         Toast.makeText(getApplicationContext(), "GO", Toast.LENGTH_SHORT).show();
@@ -335,4 +341,41 @@ public class MainActivity extends AppCompatActivity {
         Log.v(LOG_TAG, "End recognition");
     }
 
+    public void takePicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Toast.makeText(getApplicationContext(), "Working", Toast.LENGTH_SHORT).show();
+        file = Uri.fromFile(getOutputMediaFile());
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
+        startActivityForResult(intent, 100);
+
+    }
+    private File getOutputMediaFile(){
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "CameraDemo");
+
+        if (!mediaStorageDir.exists()){
+            if (!mediaStorageDir.mkdirs()){
+                Log.d("CameraDemo", "failed to create directory");
+                Toast.makeText(this,"Failed to make directory", Toast.LENGTH_SHORT).show();
+
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+        return new File(mediaStorageDir.getPath() + File.separator +
+                "IMG_"+ timeStamp + ".jpg");
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) {
+            if (resultCode == RESULT_OK) {
+                imageView.setImageURI(file);
+                Toast.makeText(getApplicationContext(),"Saved", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
